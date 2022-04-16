a8ce555e7048918776d3fc2e8154222fe1df1c76
Samiul Islam <59833987+samiul1998@users.noreply.github.com>
2022-04-17 00:04:16 +0600

    Banner Section Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Pages/Shared/Navbar/Navbar';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Home></Home>
       <h1>Welcome to Doctor Lounge</h1>
       <p>The site is under development,we will get back to you soon...</p>
       <h6>Thank You for your patience!</h6>
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Banner from './Banner/Banner';
 import './Home.css';
+
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+      <div>
+        <Banner></Banner>
+      </div>
     );
 };
 
