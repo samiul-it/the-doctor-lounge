825e29ebe6adc13be5c9eb0e2635196019766557
Samiul <samiultalukder@gmail.com>
2023-08-22 00:24:20 +0600

    Description Section Added in Home Page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,19 +7,21 @@ import "./Home.css";
 import Services from "./Services/Services";
 import BannerMUI from "./BannerMUI/BannerMUI";
 import { Box, Grid } from "@mui/material";
+import StyledDescription from "./StyledDescription/StyledDescription";
 
 const Home = () => {
   return (
     <div>
       <BannerMUI></BannerMUI>
+      <StyledDescription></StyledDescription>
 
       <Grid container justifyContent="center">
         <Box sx={{ display: "flex", pt: 2 }}>
           <h3 style={{ color: "#00adb5" }}>Our Services </h3>
         </Box>
       </Grid>
+
       <Services />
-      {/* <Description></Description> */}
     </div>
   );
 };
---
+++ src/Pages/Home/StyledDescription/StyledDescription.js
@@ -0,0 +1,124 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  Paper,
+  Typography,
+} from "@mui/material";
+import React from "react";
+
+const StyledDescription = () => {
+  const cardDescriptions = [
+    {
+      shortTitle: "A BETTER LIFE",
+      largeTitle: "Heart surgery",
+      imageLink: "https://customerthink.com/wp-content/uploads/lady-doctor.jpg",
+      description:
+        "Uniquely deploy cross-unit benefits with wireless testing procedures. Collaboratively build backward compatible relationships whereas tactical paradigms.",
+      link: "",
+      cardColor: "#de1d3c",
+    },
+    {
+      shortTitle: "A BETTER LIFE",
+      largeTitle: "Heart surgery",
+      imageLink:
+        "https://www.staffcare.com/siteassets/blogs/locum-tenens-news/building-patient-doctor-trust.jpg",
+      description:
+        "Uniquely deploy cross-unit benefits with wireless testing procedures. Collaboratively build backward compatible relationships whereas tactical paradigms.",
+      link: "",
+      cardColor: "#d51030",
+    },
+    {
+      shortTitle: "A BETTER LIFE",
+      largeTitle: "Heart surgery",
+      imageLink:
+        "https://completewellbeing.com/wp-content/uploads/2017/02/is-it-time-to-change-your-doctor.jpg",
+      description:
+        "Uniquely deploy cross-unit benefits with wireless testing procedures. Collaboratively build backward compatible relationships whereas tactical paradigms.",
+      link: "",
+      cardColor: "#c60c2a",
+    },
+  ];
+  return (
+    <Box sx={{ marginTop: { xs: 5, lg: 10 }, marginBottom: 5 }}>
+      <Grid sx={{ flexGrow: 1 }} container spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justifyContent="center" spacing="0">
+            {cardDescriptions.map((cardDescription, index) => (
+              <Grid key={index} item>
+                <Card sx={{ borderRadius: 0, maxWidth: { xs: 330, lg: 340 } }}>
+                  <CardMedia
+                    component="img"
+                    height="240"
+                    image={cardDescription.imageLink}
+                    alt="green iguana"
+                  />
+                  <CardContent
+                    sx={{
+                      height: 300,
+                      backgroundColor: cardDescription.cardColor,
+                      "&:hover": {
+                        backgroundColor: "#d51030",
+                        opacity: [0.9, 0.8, 0.7],
+                      },
+                    }}
+                  >
+                    <Typography
+                      color="#ffebee"
+                      gutterBottom
+                      variant="h6"
+                      component="div"
+                    >
+                      {cardDescription.shortTitle}
+                    </Typography>
+                    <Divider
+                      gutterBottom
+                      sx={{
+                        width: 70,
+                      }}
+                      style={{
+                        border: "none",
+                        height: 6,
+                        margin: 2,
+                      }}
+                    />
+                    <Typography
+                      color="#ffebee"
+                      gutterBottom
+                      variant="h4"
+                      component="div"
+                    >
+                      {cardDescription.largeTitle}
+                    </Typography>
+                    <Typography variant="body2" color="#ffebee">
+                      {cardDescription.description}
+                    </Typography>
+
+                    <Button
+                      style={{
+                        backgroundColor: "#ffebee",
+                        color: "#212121",
+                      }}
+                      sx={{ marginTop: 5, borderRadius: 28 }}
+                      variant="contained"
+                    >
+                      Know More
+                    </Button>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default StyledDescription;
